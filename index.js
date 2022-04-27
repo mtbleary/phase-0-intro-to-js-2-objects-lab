3632d18ea8848a24d1f5fdae0e512e5f4e3f176a
matthew leary <matt-leary@live.co.uk>
2022-04-28 00:43:35 +0100

    commit tree 378f21fc976ff0efb099fa33df2f1126eaae27dc

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee  = {
+    name: "Matt",
+    streetAddress: "2 Winchester Street"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...employee,
+        name: "Sam",
+        streetAddress: "11 Broadway"
+    }
+} 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress) {
+    employee [streetAddress] = "12 Broadway"
+    return employee
+};
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj, ...key};
+    delete newEmployee[key]
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key]
+    return obj;
+}
+
+
